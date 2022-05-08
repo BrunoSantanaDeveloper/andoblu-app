74a3d2f99f17d1481192fa65e217b052113316b3
Bruno <bsantana.it@gmail.com>
2022-05-07 21:37:19 -0300

    email text update

--- vendor/laravel/framework/src/Illuminate/Notifications/resources/views/email.blade.php
+++ vendor/laravel/framework/src/Illuminate/Notifications/resources/views/email.blade.php
@@ -4,9 +4,9 @@
 # {{ $greeting }}
 @else
 @if ($level === 'error')
-# @lang('Whoops!')
+# @lang('Opa!')
 @else
-# @lang('Hello!')
+# @lang('Olá!')
 @endif
 @endif
 
@@ -43,7 +43,7 @@
 @if (! empty($salutation))
 {{ $salutation }}
 @else
-@lang('Regards'),<br>
+@lang('Atenciosamente'),<br>
 {{ config('app.name') }}
 @endif
 
@@ -51,8 +51,8 @@
 @isset($actionText)
 @slot('subcopy')
 @lang(
-    "If you’re having trouble clicking the \":actionText\" button, copy and paste the URL below\n".
-    'into your web browser:',
+    "Se você estiver tendo problemas clicando em \":actionText\" button, copie e cole o seguinte link\n".
+    'na barra de endereço do seu navegador:',
     [
         'actionText' => $actionText,
     ]
