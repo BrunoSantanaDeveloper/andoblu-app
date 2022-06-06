5bed4393c50cab09b84b06667f76800eb40d1af8
Bruno Santana <bsantana.it@gmail.com>
2022-06-06 12:59:11 -0300

    Update dashboard-user.blade.php

--- resources/views/backend/dashboard-user.blade.php
+++ resources/views/backend/dashboard-user.blade.php
@@ -82,7 +82,7 @@
 
                             <li class="event-list {{ $transaction->trans_date == date('d/m/Y') ? 'active'}}">
                                 <div class="event-timeline-dot">
-                                    <i class="bx bxs-right-arrow-circle font-size-18 {{ $transaction->trans_date == date('d/m/Y') ? 'bx-fade-right'}} "></i>
+                                    <i class='bx bxs-right-arrow-circle font-size-18 {{ $transaction->trans_date == date("d/m/Y") ? "bx-fade-right"}} '></i>
                                 </div>
                             
                                 <div class="d-flex">
