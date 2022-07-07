5dafa93b948fb0d91b85478c77da576a4262a485
Bruno Santana <bsantana.it@gmail.com>
2022-07-07 11:31:17 -0300

    Update ExpenseController.php

--- app/Http/Controllers/ExpenseController.php
+++ app/Http/Controllers/ExpenseController.php
@@ -119,7 +119,6 @@ class ExpenseController extends Controller {
         $transaction->chart_id          = $request->input('chart_id');
         $transaction->type              = 'expense';
         $transaction->dr_cr             = 'dr';
-        //dd($request->input('amount'));
         $transaction->amount            = str_replace (',', '.', str_replace ('.', '', $request->input('amount')));
         $transaction->payer_payee_id    = $request->input('payer_payee_id');
         $transaction->payment_method_id = $request->input('payment_method_id');
@@ -218,7 +217,7 @@ class ExpenseController extends Controller {
         $transaction->chart_id          = $request->input('chart_id');
         $transaction->type              = 'expense';
         $transaction->dr_cr             = 'dr';
-        $transaction->amount            = $request->input('amount');
+        $transaction->amount            = str_replace (',', '.', str_replace ('.', '', $request->input('amount')));
         $transaction->payer_payee_id    = $request->input('payer_payee_id');
         $transaction->payment_method_id = $request->input('payment_method_id');
         $transaction->reference         = $request->input('reference');
