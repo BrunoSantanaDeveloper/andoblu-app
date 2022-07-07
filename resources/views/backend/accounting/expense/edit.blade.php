3252bbbba0f3765a7c18f610b39f23ec97565cc5
Bruno Santana <bsantana.it@gmail.com>
2022-07-07 15:03:22 -0300

    Update edit.blade.php

--- resources/views/backend/accounting/expense/edit.blade.php
+++ resources/views/backend/accounting/expense/edit.blade.php
@@ -117,7 +117,7 @@
                         <div class="col-md-6 clear">
                         <div class="form-check form-switch form-switch-lg mb-3" dir="ltr">
                                                 <input class="form-check-input" type="checkbox" id="SwitchCheckSizelg" checked="">
-                                                <label class="form-check-label" for="SwitchCheckSizelg">Large Size Switch</label>
+                                                <label class="form-check-label" for="SwitchCheckSizelg">Autorizar Pagamento</label>
                                             </div>
                         </div>
 
