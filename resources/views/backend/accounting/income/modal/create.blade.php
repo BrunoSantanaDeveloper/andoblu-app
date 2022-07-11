69dc6322999adaa4e48f3f04f93b7162f3833d27
Bruno Santana <bsantana.it@gmail.com>
2022-07-11 19:59:33 -0300

    Update create.blade.php

--- resources/views/backend/accounting/income/modal/create.blade.php
+++ resources/views/backend/accounting/income/modal/create.blade.php
@@ -31,7 +31,7 @@
                         class="ti-plus"></i> {{ _lang('Add New') }}</a>
                 <label class="control-label">{{ _lang('Income Type') }}</label>
                 <select class="form-control select2-ajax" data-value="id" data-display="name"
-                    data-table="chart_of_accounts" data-where="3" name="chart_id" required>
+                    data-table="chart_of_accounts" data-where="3" name="chart_id" >
                     <option value="">{{ _lang('Select One') }}</option>
                     {{ create_option("chart_of_accounts","id","name",old('chart_id'),array("type="=>"income","AND company_id="=>company_id())) }}
                 </select>
@@ -66,14 +66,14 @@
                         class="ti-plus"></i> {{ _lang('Add New') }}</a>
                 <label class="control-label">{{ _lang('Payment Method') }}</label>
                 <select class="form-control select2-ajax" data-value="id" data-display="name"
-                    data-table="payment_methods" data-where="1" name="payment_method_id" required>
+                    data-table="payment_methods" data-where="1" name="payment_method_id" >
                     <option value="">{{ _lang('Select One') }}</option>
                     {{ create_option("payment_methods","id","name",old('payment_method_id'),array("company_id="=>company_id())) }}
                 </select>
             </div>
         </div>
 
-        <div class="col-md-12">
+        <div class="col-md-6">
             <div class="form-group">
                 <label class="control-label">{{ _lang('Reference') }}</label>
                 <input type="text" class="form-control" name="reference" value="{{ old('reference') }}">
